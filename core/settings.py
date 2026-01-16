f65b35ab9fa2c40082ca3d809e5d6e0f4a0c3ab1
NITHIN-NT <nithinraj07sachu@gmail.com>
2026-01-16 16:05:24 +0530

    change : db changed to postgres

--- core/settings.py
+++ core/settings.py
@@ -1,6 +1,8 @@
 import os
 import environ
 from pathlib import Path
+import dj_database_url
+
 
 # Initialize environ
 env = environ.Env(
@@ -87,10 +89,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL"),
+        conn_max_age=60,      
+        ssl_require=False,
+    )
 }
 
 
